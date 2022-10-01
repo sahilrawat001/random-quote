5417206c34ea9987dbafdcb38263a0d270037461
Arturs Smirnovs <yam.aka.as@gmail.com>
2022-10-01 21:17:40 +0300

    Fixed title / fixed position center always / small code cleanup

--- src/quoteBox/Quote.jsx
+++ src/quoteBox/Quote.jsx
@@ -4,12 +4,12 @@ import './Quote.css';
 const Quote = ({items }) => {
     return (
         <div className='quotebox'>
-            <h1 className='quotebox-quote' >
+            <h1 className='quotebox-quote'>
                 {items.advice}
             </h1>
         </div>
     )    
 }
 
- export default Quote;
+export default Quote;
  
